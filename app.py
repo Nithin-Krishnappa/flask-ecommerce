cea784c12843dd10bad6b81544a2648eb67d17f7
Nithin k <141625987+Nithin-Krishnappa@users.noreply.github.com>
2024-12-10 01:33:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,11 +26,7 @@ db = SQLAlchemy(app)
 # Import the Product model
 from model import Product
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.String(200), nullable=True)
+
 
 
 # Routes
