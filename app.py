bad5192b4ba60c0b06867b017e0d5a835990f655
Nithin k <141625987+Nithin-Krishnappa@users.noreply.github.com>
2024-12-10 01:18:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from . import Product
+
 
 
 app = Flask(__name__)
